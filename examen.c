8b9dc52c46096123e16e97696a3483fe0e6188fd
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-02 12:53:58 -0500

    commit5

--- examen.c
+++ examen.c
@@ -5,32 +5,31 @@ porcentaje de números negativos.
 */
 
 
-# include <stdio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     int n, num, ceros = 0, pos = 0, neg = 0;
     float porc_ceros, porc_pos, porc_neg;
 
-    //pedimos al usuario que ingrese la cantidad de numeros a evaluar
-
-    printf("ingrese la cantidad de numeros a evaluar: ");
-    scanf("%d, &n");
+    // Pedimos al usuario que ingrese la cantidad de números a evaluar
+    printf("Ingrese la cantidad de numeros a evaluar: ");
+    scanf("%d", &n);
 
     // Pedimos al usuario que ingrese los N números
-
-    printf("Ingrese los %d números:\n", n);
+    printf("Ingrese los %d numeros:\n", n);
 
     for (int i = 1; i <= n; i++) {
-    printf("Número %d: ", i);
-    scanf("%d", &num);
-    // Verificamos si el número es cero, positivo o negativo y lo contabilizamos
-    if (num == 0) {
-    ceros++;
-    } else if (num > 0) {
-    pos++;
-    } else {
-    neg++;
-    }
+        printf("Número %d: ", i);
+        scanf("%d", &num);
+
+        // Verificamos si el número es cero, positivo o negativo y lo contabilizamos
+        if (num == 0) {
+            ceros++;
+        } else if (num > 0) {
+            pos++;
+        } else {
+            neg++;
+        }
     }
 
     // Calculamos los porcentajes de ceros, positivos y negativos
@@ -40,11 +39,10 @@ int main()
 
     // Mostramos los resultados al usuario
     printf("\nResultados:\n");
-    printf("Porcentaje de números cero: %.2f%%\n", porc_ceros);
-    printf("Porcentaje de números positivos: %.2f%%\n", porc_pos);
-    printf("Porcentaje de números negativos: %.2f%%\n", porc_neg);
+    printf("Porcentaje de numeros cero: %.2f%%\n", porc_ceros);
+    printf("Porcentaje de numeros positivos: %.2f%%\n", porc_pos);
+    printf("Porcentaje de numeros negativos: %.2f%%\n", porc_neg);
 
     return 0;
 }
-    
 
