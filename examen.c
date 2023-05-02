304b5663864b234044a76df978d532511253318d
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-02 12:33:33 -0500

    commit3

--- examen.c
+++ examen.c
@@ -1,6 +1,16 @@
 /*
-
+Construya un programa para hallar, de un conjunto de N números, 
+qué porcentaje son cero, qué porcentaje son positivos y el 
+porcentaje de números negativos.
 */
 
 
 # include <stdio.h>
+int main()
+{
+
+
+
+
+    
+}
