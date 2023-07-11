3775df22035614b8ac10fc6ced2068e5827e54ac
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-07-11 12:23:14 -0500

    definir las bibliotecas

--- examen.c
+++ examen.c
@@ -6,4 +6,16 @@ escriba el nuevo texto corregido en otro archivo diferente.
  Además, muestre en pantalla cuantas palabras se corrigió 
  en total. Debe utilizar el concepto de "Función". */
 
- 
+ #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORD_SIZE 20
+
+void capitalize(char *word) {
+    if (word[0] >= 'a' && word[0] <= 'z') {
+        word[0] = toupper(word[0]);
+    }
+}
+
